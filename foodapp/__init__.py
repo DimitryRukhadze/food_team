27f78a9abed746b32901d992bec961fdb6df033c
aosothra <arhdam@yandex.ru>
2022-03-31 19:35:22 +0400

    Fix UTF conversion to ASCII escape characters

--- foodapp/__init__.py
+++ foodapp/__init__.py
@@ -11,6 +11,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'db.json'),
+        JSON_AS_ASCII=False
     )
 
     if test_config is None:
