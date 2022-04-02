4c5e83ccc439b532861663dddb9dcef7d7be4521
Govno <arhdam@yandex.ru>
2022-04-02 22:02:50 +0400

    Add recipe fetching

--- foodapp_api.py
+++ foodapp_api.py
@@ -65,6 +65,29 @@ def get_user_api(chat_id):
     return response.json()['id']
 
 
+def get_pricing_api():
+    url = 'http://127.0.0.1:5000/api/getPlans'
+
+    response = requests.post(url)
+    response.raise_for_status()
+
+    return response.json()['plans']
+
+
+def get_recipe(sub_id):
+    url = 'http://127.0.0.1:5000/api/getRecipe'
+    headers = {'Content-Type': 'application/json'}
+    json_data = {
+        'bot_token':'123',
+        'sub_id': sub_id
+    }
+
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+
+    return response.json()['recipe']
+
+
 def main():
     pass
 
