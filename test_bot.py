0cc43c9f74c954f11c0427ee3d6584660f8d67ba
Stanislav <77130336+standbit@users.noreply.github.com>
2022-04-03 19:38:34 +0300

    Give user the recipe

--- test_bot.py
+++ test_bot.py
@@ -104,10 +104,15 @@ def show_subscriptions(update, context) -> None:
     else:
         query.edit_message_text(text="Подписок не найдено. Вернитесь в стартовое меню и оформите подписку.")
 
+
 def give_user_recipe(update, context):
     chat_id = update.effective_chat.id
-
     recipe = foodapp_api.get_recipe(int(update.callback_query.data))
+    text, image_url = show_recipe(recipe)
+    update.message.reply_markdown_v2("*Ваше блюдо ↓*")
+    with open(image_url, "rb") as photo:
+        update.message.reply_photo(photo)
+    update.message.reply_markdown_v2(text)
 
 
 def get_menu_type(update, context):
